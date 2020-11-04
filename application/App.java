1ed8e46891c56937c8a770c7db68bbdbe72da063
Derek Vukusich <dvukusich@gmail.com>
2020-11-04 13:30:08 -0700

    initial commit

---
+++ application/App.java
@@ -0,0 +1,22 @@
+package application;
+
+public class App {
+
+
+	/*
+	 * Coding Steps: This week you will be working together as a team to create a full CRUD application. 
+	 * Your console CRUD application will need to use a database to store all the application data. 
+	 * As a team, decide what you want your project to do. 
+	 * Get instructor approval early in the week before beginning development. 
+	 * You need to have at least 3 entities. 
+	 * Users should be able to interact via the console (i.e. Scanner(System.in))) 
+	 * Use git to collaborate.
+	 */	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
